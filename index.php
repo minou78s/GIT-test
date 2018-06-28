6f11d52b7dd328632ab7592596824630965a0c63
Amine SAOUDI <amine5559@hotmail.fr>
2018-06-28 11:24:18 +0200

    modif h1

--- index.php
+++ index.php
@@ -5,6 +5,23 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>GIT test</title>
+    <style>
+    h1 {
+    font-family: Dusha;
+    position: absolute;
+    top: 40%;
+    left: 25%;
+    color: blue;
+    font-size: 4em;
+
+    }
+
+
+    @font-face {
+    font-family: Dusha;
+    src: url(Dusha.ttf);
+    }
+    </style>
 </head>
 <body>
     <h1>Vivement le mechoui ce midi!!</h1>
